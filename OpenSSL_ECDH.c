c15a1c12e0e1072928b71cadbd09d13c4dea57ac
Brandc <Gate2TheStars@gmail.com>
2015-07-29 03:47:24 -0700

    Added example

---
+++ OpenSSL_ECDH.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <openssl/ec.h>
+#include <openssl/ecdh.h>
+/*NID_X9_62_prime256v1*/
+#include <openssl/evp.h>
+
+/*Nice little macro to save a few lines.*/
+void die(char *reason)
+{
+    fprintf(stderr, reason);
+    fflush(stderr);
+    exit(1);
+}
+
+/*Elliptic Curve Diffie-Hellman function*/
+int EC_DH(unsigned char **secret, EC_KEY *key, const EC_POINT *pPub)
+{
+    int secretLen;
+
+    secretLen = EC_GROUP_get_degree(EC_KEY_get0_group(key));
+    secretLen = (secretLen + 7) / 8;
+
+    *secret = malloc(secretLen);
+    if (!*secret)
+        die("Failed to allocate memory for secret.\n");
+    secretLen = ECDH_compute_key(*secret, secretLen, pPub, key, NULL);
+
+    return secretLen;
+}
+
+/*Key generation function for throwaway keys.*/
+EC_KEY* gen_key(void)
+{
+    EC_KEY *key;
+
+    key = EC_KEY_new_by_curve_name(NID_X9_62_prime256v1);
+    if (key == NULL)
+        die("Failed to create lKey object.\n");
+
+    if (!EC_KEY_generate_key(key))
+        die("Failed to generate EC key.\n");
+
+    return key;
+}
+
+int main(int argc, char **argv)
+{
+    EC_KEY *lKey, *pKey;
+    int lSecretLen, pSecretLen;
+    unsigned char *lSecret, *pSecret;
+
+    lKey = gen_key();
+    pKey = gen_key();
+
+    lSecretLen = EC_DH(&lSecret, lKey, EC_KEY_get0_public_key(pKey));
+    pSecretLen = EC_DH(&pSecret, pKey, EC_KEY_get0_public_key(lKey));
+    if (lSecretLen != pSecretLen)
+        die("SecretLen mismatch.\n");
+
+    if (memcmp(lSecret, pSecret, lSecretLen))
+        die("Secrets don't match.\n");
+
+    free(lSecret);
+    free(pSecret);
+    EC_KEY_free(lKey);
+    EC_KEY_free(pKey);
+    CRYPTO_cleanup_all_ex_data();
+
+    return 0;
+}
+
+
+
+
+
+
+
